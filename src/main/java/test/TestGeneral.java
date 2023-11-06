eb5c1b1206fb9596524fb8d5887a69bf4b85da36
Aldo Estrada <63128771+aldo-estrada@users.noreply.github.com>
2023-11-06 12:11:06 -0800

    Only testing 2 tests

--- src/main/java/test/TestGeneral.java
+++ src/main/java/test/TestGeneral.java
@@ -6,12 +6,18 @@ import GSLO.GlobalSearch;
 import baseline.greedy.*;
 import baseline.skater.ModifiedSKATER;
 import baseline.skatercon.ModifiedSKATERCON;
+import com.google.common.geometry.S2LatLng;
+import com.google.common.geometry.S2Loop;
+import com.google.common.geometry.S2Point;
+import com.google.common.geometry.S2Polygon;
+import org.locationtech.jts.geom.Coordinate;
 import util.Area;
 import util.Preprocess;
 import util.Region;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestGeneral {
 
@@ -19,9 +25,7 @@ public class TestGeneral {
     public static void main(String args[]) throws InterruptedException, CloneNotSupportedException, IOException {
         
 
-
-
-        int GSLO_iter = 100;
+        int GSLO_iter = 10; // changed to 10 from 100
         int iter_skatercon = 100;
         int iter_prob = 100;
         double scale = 0.02;
@@ -51,26 +55,25 @@ public class TestGeneral {
         System.out.println("18.test the scalability of GSLO, SKATER* and SKATERCON* under different sizes of dataset in the TIGER");
 
 
-        var_iter_seed_quality(GSLO_iter , scale , default_p  , "2k");
-        test_time_breakdown(GSLO_iter ,  scale , "2k" );
-        greedy_baseline(GSLO_iter , scale , "island" , true);
-        greedy_baseline(GSLO_iter , scale , "diversity" , false);
-        var_max_no_move(GSLO_iter , scale ,  "2k");
-        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , scale , "2k");
-        baseline_GS(GSLO_iter , scale , "2k");
-        baseline_GS(GSLO_iter, scale , "diversity");
-        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , scale , "diversity");
-        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , 0 , "2k");
-        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , 0 , "diversity");
-        var_thre_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , default_p , scale , "2k");
-        var_thre_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , default_p , scale , "diversity");
-        var_p_finding_feasible_partition(iter_prob , scale , "2k");
-        var_p_finding_feasible_partition(iter_prob , scale , "diversity");
-        var_thre_finding_feasible_partition(iter_prob , scale , "2k");
-        var_thre_finding_feasible_partition(iter_prob , scale , "diversity");
-        var_dataset_hetero_runtime(iter_scalable_test , default_p , iter_scalable_test , iter_skatercon,  scale);
+   //     var_iter_seed_quality(GSLO_iter , scale , default_p  , "2k");
+   //     test_time_breakdown(GSLO_iter ,  scale , "2k" );
+//        greedy_baseline(GSLO_iter , scale , "island" , true);
+//        greedy_baseline(GSLO_iter , scale , "diversity" , false);
+  //      var_max_no_move(GSLO_iter , scale ,  "2k");
+        var_p_measure_hetero_runtime(GSLO_iter , 0 , 0 , scale , "5k");
+      //  baseline_GS(GSLO_iter , scale , "2k");
+//        baseline_GS(GSLO_iter, scale , "diversity");
+//        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , scale , "diversity");
+//        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , 0 , "2k");
+//        var_p_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , 0 , "diversity");
+        var_thre_measure_hetero_runtime(GSLO_iter , 0 , 0 , default_p , scale , "5k");
+//        var_thre_measure_hetero_runtime(GSLO_iter , skater_iter , iter_skatercon , default_p , scale , "diversity");
+//        var_p_finding_feasible_partition(iter_prob , scale , "2k");
+//        var_p_finding_feasible_partition(iter_prob , scale , "diversity");
+//        var_thre_finding_feasible_partition(iter_prob , scale , "2k");
+//        var_thre_finding_feasible_partition(iter_prob , scale , "diversity");
+//        var_dataset_hetero_runtime(iter_scalable_test , default_p , iter_scalable_test , iter_skatercon,  scale);
 
-        
         System.out.println("----------          experiment ends        ----------");
 
     }
